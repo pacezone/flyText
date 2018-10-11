40b6be5b957a6cdfe805e70d55a2033805d1a1d7
pace <pace621@outlook.com>
2018-10-12 00:49:58 +0800

    try

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="hello">
+    <fly :list='list'></fly>
+  </div>
+</template>
+
+<script>
+import fly from './fly'
+export default {
+  name: 'HelloWorld',
+  components: {
+    fly
+  },
+  data () {
+    return {
+      list:[
+        {name:'第一条乱飞的数据'},
+        {name:'第一发大水发的'},{
+          name:'对方是否都感受到法国'
+        },{
+          name:'东方饭店更舒服的'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/fly.vue
@@ -0,0 +1,69 @@
+<template>
+  <div ref="fly-container" class="content">
+    <template v-for="(item,index) in newList">
+      <span :style="{top:item.y+'px',left:item.x+'px'}">
+        {{item.text}}
+      </span>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  name:"fly",
+  props:{
+    list:{
+      type:Array,
+      default:[]
+    }
+  },
+  data () {
+    return {
+      newList:[],
+      cWidth:null,
+      cHeight:null
+    }
+  },
+  created () {
+    this.$nextTick(()=>{
+      this.initData()
+      this.anit()
+    })
+  },
+  methods: {
+    initData(){
+      this.cWidth = this.$refs['fly-container'].offsetWidth
+      this.cHeight = this.$refs['fly-container'].offsetHeight
+
+      this.list.forEach(item => {
+        this.newList.push({x:this.cWidth*(Math.random()),y:this.cHeight*(Math.random()),text:item.name})
+      })
+    },
+    anit(){
+      let timer = null
+      timer = setInterval(()=>{
+        this.newList.forEach(i=>{
+          this.changeXY(i)
+        })
+      },100)
+      this.newList.forEach()
+    },
+    changeXY(i){
+      i.x++
+      i.y++
+    }
+  }
+}
+</script>
+<style>
+  .content {
+    width: 80%;
+    height: 300px;
+    margin: 0 auto;
+    position: relative;
+  }
+  .content span {
+    position: absolute;
+  }
+</style>
+
+
